e60592401e46f51847e951233860d7bba642f76a
Daryn Arakawa <daryn.arakawa@gmail.com>
2017-05-10 05:07:13 -0700

    New adds

--- Real_Time_trial/test_neural_network.c
+++ Real_Time_trial/test_neural_network.c
@@ -6,11 +6,29 @@
 #include "fann.h"
 #include "constants.h"
 
+void create_names(char *name, char ***c){
+    char **dot_net_name;
+    dot_net_name = (char **) malloc(sizeof(char*) * (ACTIVITIES + 1));
+
+    for (i = 0; i < ACTIVITIES + 1; i++) {
+        dot_net_name[i] = (char *) malloc(sizeof(char) * BUFF_SIZE);
+
+        memset(dot_net_name[i], 0, sizeof(char) * BUFF_SIZE);
+
+    sprintf(dot_net_name[0], "%s%s_%s/%s_%s.net", main_path, train_file, name, activities_file, name);
+    
+    for (i = 0; i < ACTIVITIES; i++) {
+        sprintf(dot_net_name[i+1], "%s%s_%s/%s_%s.net", main_path, train_file, name, train_speeds[i], name);
+    }
+
+    *c = dot_net_name;
+}
+
 
 void test_neural_network(float **class_v, int num_rows_classifiers, char* name){
     int i, j, motion, speed;
     float max;
-    
+
     //names of files
     char* TRAINING = (char*)malloc(sizeof(char)*1024);
     char* WALKING = (char*)malloc(sizeof(char)*1024);
@@ -18,18 +36,22 @@ void test_neural_network(float **class_v, int num_rows_classifiers, char* name){
     char* JUMPING = (char*)malloc(sizeof(char)*1024);
     char* STAIR_ASCENT = (char*)malloc(sizeof(char)*1024);
     char* STAIR_DESCENT = (char*)malloc(sizeof(char)*1024);
+    char *TURNING_LEFT = (char*)malloc(sizeof(char)*1024);
+    char *TURNING_RIGHT = (char*)malloc(sizeof(char)*1024);
 
     sprintf(TRAINING,"TRAINING_%s.net", name);
     sprintf(WALKING,"WALKING_%s.net", name);
     sprintf(RUNNING,"RUNNING_%s.net", name);
     sprintf(JUMPING,"JUMPING_%s.net", name);
     sprintf(STAIR_ASCENT,"STAIR_ASCENT_%s.net", name);
-    sprintf(STAIR_DESCENT,"STAIR_DESCENT_%s.net", name);    
+    sprintf(STAIR_DESCENT,"STAIR_DESCENT_%s.net", name);
+    sprintf(TURNING_LEFT,"TURNING_LEFT_%s.net", name);
+    sprintf(TURNING_RIGHT,"TURNING_RIGHT_%s.net", name);
     
     // calc_out returns an array of outputs, with the number of outputs being equal to number 
     // of neurons in output layer
     fann_type *calc_out_motion, *calc_out_speed;
-    fann_type input[NUM_CLASSIFIERS];
+    fann_type input[FEATURES];
     
     // Our artificial neural networks for motions and speeds
     struct fann *motion_ann, 
@@ -55,7 +77,7 @@ void test_neural_network(float **class_v, int num_rows_classifiers, char* name){
 
         // Populate our input array in preparation for neural network
         for (j=0; j < NUM_CLASSIFIERS; j++){
-            input[j] = class_v[i];
+            input[j] = class_v[i][j];
         }
 
         calc_out_motion = fann_run(motion_ann, input);
@@ -63,10 +85,17 @@ void test_neural_network(float **class_v, int num_rows_classifiers, char* name){
         /* Check for each motion */
         for (j = 0; j < NUM_MOTIONS; j++) {
 
+            /* Note that calc_out_motion[j] will always return a value between -1 and 1 */
             if (calc_out_motion[j] > max) {
                 max = calc_out_motion[j];
                 motion = j;
             }
+        }
+
+        // Motion is not convincingly classified; assume we are doing nothing
+        if (max < CALC_OUT_THRESHOLD){
+            printf("Currently doing nothing.");
+            continue;
         }
 
         /* Now that motion is determined, find speed */
